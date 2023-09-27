e71eb8c01512fd4c4bfcd2d86d6a4ccdacc0f94f
Shahrukh <Shahrukh@MacBook-Pro-2.local>
2023-09-27 13:04:35 +0530

    update data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import CartCard from "./Components/CartCard/CartCard";
 import store from "./utils/store";
 import { Provider } from "react-redux";
 import { createMulitSelect } from "./utils/helper";
-import { COLD_APPETIZERS } from "./data/restaurantData";
+import {
+  COLD_APPETIZERS,
+  HOT_APPETIZERS,
+  SANDWICH,
+  PLATE_ON_OVEN,
+  SOUP,
+  SHAWARMA,
+  GRILLED,
+} from "./data/restaurantData";
 
 function App() {
   const [selectedCard, setSelectedCard] = React.useState(
-    createMulitSelect(COLD_APPETIZERS)
+    createMulitSelect([
+      ...COLD_APPETIZERS,
+      ...HOT_APPETIZERS,
+      ...SANDWICH,
+      ...PLATE_ON_OVEN,
+      ...SOUP,
+      ...SHAWARMA,
+      ...GRILLED,
+    ])
   );
   return (
     <Provider store={store}>
